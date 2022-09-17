521c6af42c8ecb760e9d854c96f01788a0a7de1d
kaufmanndevgit <stephaniekaufmann02@gmail.com>
2022-09-17 18:41:50 -0300

    exercicio_html_js

--- main.js
+++ main.js
@@ -42,3 +42,22 @@ nomeBeneficiario.addEventListener('Keyup', function(e) {
     }
 });
 
+const campoA = document.getElementById('numbera');
+const campoB = document.getElementById('numberb');
+
+
+    form.addEventListener('submit', function (a){
+
+        const mensagemCerto = `Sucesso! o valor: ${campoB.value}  do campo B esta maior que o valor : ${campoA.value} do campo A.`;
+        const e = `Errado, o valor: ${campoA.value} do campo A esta maior ou igual ao valor: ${campoB.value} do campo B`;
+     
+
+            if (campoA.value > campoB.value || campoA.value === campoB.value){
+                alert(e);           
+            }else {
+                alert(mensagemCerto);
+            }
+        });
+    
+console.log(form);
+a.preventDefault();
