de97fd7359b7b8dc3b7eb7ba545e979091c91e35
kaufmanndevgit <stephaniekaufmann02@gmail.com>
2022-10-05 13:19:32 -0300

    exercicio_plugins_jquery

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+$("#telefone").mask("(00) 0000-00009")
+
+
+$('#cpf').mask('000000000-00', {
+    placeholder:'000000000-00'
+})
+
+$('#cep').mask('00000-000', {
+    placeholder:'00000-000'
+})
+})
